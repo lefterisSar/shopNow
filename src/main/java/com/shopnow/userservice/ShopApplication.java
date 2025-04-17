4f56117674dcb83cd8ec7efddd5c177702f84b11
Lefteris Saridakis <saridakiselef@hotmail.com>
2025-04-17 18:53:28 +0300

    Add support for externalized datasource configuration
    Introduce `DatasourceProperties` class to manage datasource configuration using Spring Boot's properties binding. Integrate dotenv library to load environment variables and set system properties for the datasource in `ShopApplication`. Update POM to include a dependency for dotenv-java.

--- src/main/java/com/shopnow/userservice/ShopApplication.java
+++ src/main/java/com/shopnow/userservice/ShopApplication.java
@@ -1,15 +1,22 @@
 package com.shopnow.userservice;
 
+import com.shopnow.userservice.config.DatasourceProperties;
 import com.shopnow.userservice.config.JwtProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, DatasourceProperties.class})
 @SpringBootApplication
 public class ShopApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         SpringApplication.run(ShopApplication.class, args);
     }
 
---
+++ src/main/java/com/shopnow/userservice/config/DatasourceProperties.java
@@ -0,0 +1,23 @@
+package com.shopnow.userservice.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DatasourceProperties {
+    private String url;
+    private String username;
+    private String password;
+
+    @PostConstruct
+    public void checkEnv() {
+        System.out.println("SPRING_DATASOURCE_PASSWORD (System): " + System.getProperty("SPRING_DATASOURCE_PASSWORD"));
+        System.out.println("SPRING_DATASOURCE_USERNAME (System): " + System.getProperty("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("SPRING_DATASOURCE_URL (System): " + System.getProperty("SPRING_DATASOURCE_URL"));
+    }
+
+}
